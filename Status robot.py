7befa07b8b12eb37de97646b1863d46d48677034
DetoxY57 <titi.herbivo@gmail.com>
2025-05-06 14:05:24 +0200

    Add files via upload

---
+++ Status robot.py
@@ -0,0 +1,19 @@
+﻿import os
+import json
+import time
+
+def ping(ip):
+    response = os.system("ping -n 1 {}".format(ip))  # pour Windows
+    return response == 0
+
+while True:
+    status = {
+        "R1": "🟢 En marche" if ping("172.25.214.61") else "🔴 Hors ligne",
+        "R2": "🟢 En marche" if ping("172.25.214.62") else "🔴 Hors ligne",
+        "R3": "🟡 Maintenance"
+    }
+
+    with open("status.json", "w") as f:
+        json.dump(status, f)
+
+    time.sleep(5)  # comme une pause cyclique d'OB
